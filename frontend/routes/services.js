bd454ccc57e95836798b98f148c28887fdfb9b86
Ramon Baier <158073292+RamonBaier@users.noreply.github.com>
2025-05-22 13:34:56 +0000

    listagem de pets, tutors, products, services aplicada

--- frontend/routes/services.js
+++ frontend/routes/services.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var url = process.env.URL_API
 
-router.get('/', function(req, res, next) {
-  res.render('layout/layout', { 
+
+router.get('/', async function(req, res, next) {
+  try {
+
+    const servicesResponse =  await fetch(`${url}/services`);
+    const services =  await servicesResponse.json();
+
+    res.render('layout/layout', { 
     body:'../pages/services', 
-    title: 'Serviços' 
+    title: 'Serviços' ,
+    services : services
   });
+  } catch (err) {
+    console.log(err)
+    res.status(500).send("Erro ao buscar serviços")
+  }
+
+  
 });
 
 module.exports = router;
