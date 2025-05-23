8f54e56e77a5d7fa45d87b9406099d0d4853cbfc
itech <chvasconcelos@yahoo.com>
2025-05-23 14:41:17 -0300

    atualizando tutors

--- frontend/routes/tutors.js
+++ frontend/routes/tutors.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
+const url = "https://hq90rqnv-3000.brs.devtunnels.ms"
 
-router.get('/', function(req, res, next) {
-  res.render('layout', { body:'pages/tutors', title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    const productsResponse = await fetch(`${url}/products`);
+    console.log('Status da resposta de produtos:', productsResponse.status);
+    if (!productsResponse.ok) throw new Error('Erro ao buscar produtos');
+
+    const products = await productsResponse.json();
+
+    res.render('pages/tutors', { 
+      title: 'Tutores'
+    });
+  } catch (error) {
+    console.error('Erro ao carregar tutores:', error.message);
+    res.status(500).send('Erro no servidor');
+  }
 });
 
 module.exports = router;
