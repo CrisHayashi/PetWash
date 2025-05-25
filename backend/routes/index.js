d037529e50286f74a78b050c98a6d6450fac6cff
Eduarda <es200783@gmail.com>
2025-05-25 14:02:32 +0000

    modificações do index para o backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,17 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({ message: 'Bem vindo a API PetWash!' });
-});
-
-router.get('/partials/:name', (req, res, next) => {
-  const name = req.params.name;
-  const filePath = path.join(__dirname, '../views/partials', `${name}.ejs`);
-
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) return next(); // se o arquivo não existir, vai para o 404
-    res.render(`partials/${name}`);
-  });
+  res.render('index');
 });
 
 module.exports = router;
